c211e866bc7c846194f2d4c49f21da2b88d958e0
HENDRIK <hendrik@KALI.home>
2020-09-19 19:00:48 +0200

    init

---
+++ hello-world/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hello_world
+{
+     class Program
+    {
+        private static void Main(string[] args)//private is set since the variables are never public, this is safer
+        {
+            var names = new List<string> { "World", "Hendrik", "Felipe", "Emilia", "Moira" };//List had to be imported
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+    }
+}
